dbad69d681e6ee73d0661e10144353f08f34c386
Vladimir <netqualityteam@gmail.com>
2021-10-12 12:04:50 +0300

    first commit

---
+++ CameraAPP/CameraAPP/Model/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CameraAPP.Model
+{
+    public class Camera
+    {
+
+        public string Name { get; set; }
+        public bool IsSoundOn { get; set; }
+        public bool IsRecordingOn { get; set; }
+    }
+}
---
+++ CameraAPP/CameraAPP/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CameraAPP.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraAPP.ViewModel
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public List<Camera> CameraList { get; set; }
+
+        public ApplicationViewModel()
+        {
+            CameraList = new List<Camera>();
+            for (int i = 0; i < 5; i++)
+            {
+                CameraList.Add(new Camera
+                {
+                    Name = "camera-" + i,
+                    IsSoundOn = true,
+                    IsRecordingOn = false
+                });
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
